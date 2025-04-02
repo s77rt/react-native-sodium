1c5d7ff7b6655c5960a903045c60e650d82c8d8d
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-04-02 19:55:57 +0100

    move sodiuma_ho into sodiuma

--- cpp/sodiuma.cpp
+++ cpp/sodiuma.cpp
@@ -1,20 +1,37 @@
 #include "sodiuma.h"
-#include "sodiuma_ho.h"
 
 #include <sodium.h>
 
 namespace s77rt {
 namespace sodiuma {
 
+Sodiuma::~Sodiuma() {}
+
+facebook::jsi::Value Sodiuma::get(facebook::jsi::Runtime &,
+                                  const facebook::jsi::PropNameID &name) {
+  return facebook::jsi::Value(5);
+}
+
+void Sodiuma::set(facebook::jsi::Runtime &,
+                  const facebook::jsi::PropNameID &name,
+                  const facebook::jsi::Value &value) {
+  return;
+}
+
+std::vector<facebook::jsi::PropNameID>
+Sodiuma::getPropertyNames(facebook::jsi::Runtime &rt) {
+  std::vector<facebook::jsi::PropNameID> result;
+  return result;
+}
+
 void Install(facebook::jsi::Runtime &runtime) {
   if (sodium_init() < 0) {
     throw std::runtime_error{"Failed to initialize sodium"};
   }
 
-  const auto sodiumaHostObject = facebook::jsi::Object::createFromHostObject(
-      runtime, std::make_shared<SodiumaHostObject>());
   runtime.global().setProperty(runtime, "__s77rt__sodiuma",
-                               std::move(sodiumaHostObject));
+                               facebook::jsi::Object::createFromHostObject(
+                                   runtime, std::make_shared<Sodiuma>()));
 }
 
 } // namespace sodiuma
--- cpp/sodiuma.h
+++ cpp/sodiuma.h
@@ -5,6 +5,19 @@
 namespace s77rt {
 namespace sodiuma {
 
+// Sodiuma Host Object
+class Sodiuma : public facebook::jsi::HostObject {
+public:
+  ~Sodiuma();
+  facebook::jsi::Value get(facebook::jsi::Runtime &,
+                           const facebook::jsi::PropNameID &name);
+  void set(facebook::jsi::Runtime &, const facebook::jsi::PropNameID &name,
+           const facebook::jsi::Value &value);
+  std::vector<facebook::jsi::PropNameID>
+  getPropertyNames(facebook::jsi::Runtime &rt);
+};
+
+// Initialize libsodium and inject sodiuma object into js globals
 void Install(facebook::jsi::Runtime &runtime);
 
 } // namespace sodiuma
