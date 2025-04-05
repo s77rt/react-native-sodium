f957f41527024d09bd57bf876eaef399fe561376
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-04-05 23:04:38 +0100

    add type to export sodium object

--- js/index.ts
+++ js/index.ts
@@ -5,6 +5,46 @@ if (!globalThis.__s77rt__sodiuma) {
 	console.error("[@s77rt/react-native-sodium] sodiuma object not found");
 }
 
+type Sodium = {
+	randombytes_random: () => number;
+	randombytes_uniform: (upperBound: number) => number;
+	randombytes_buf: (buf: ArrayBuffer, size: number) => void;
+	randombytes_buf_deterministic: (
+		buf: ArrayBuffer,
+		size: number,
+		seed: ArrayBuffer
+	) => void;
+	randombytes_close: () => number;
+	randombytes_stir: () => void;
+	crypto_generichash: (
+		output: ArrayBuffer,
+		outputLen: number,
+		input: ArrayBuffer,
+		inputLen: number,
+		key: ArrayBuffer,
+		keyLen: number
+	) => number;
+	crypto_generichash_init: (
+		state: Record<string, never>,
+		key: ArrayBuffer,
+		keyLen: number,
+		outputLen: number
+	) => number;
+	crypto_generichash_update: (
+		state: Record<string, never>,
+		input: ArrayBuffer,
+		inputLen: number
+	) => number;
+	crypto_generichash_final: (
+		state: Record<string, never>,
+		output: ArrayBuffer,
+		outputLen: number
+	) => number;
+	crypto_generichash_keygen: (k: ArrayBuffer) => void;
+};
+
 // TODO(s77rt): Copy & cache host object properties
 
-export default globalThis.__s77rt__sodiuma;
+const sodium: Sodium = globalThis.__s77rt__sodiuma;
+
+export default sodium;
