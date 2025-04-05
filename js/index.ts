74b0f8375c3a2af762c92b944727bf1965fe9ee0
Abdelhafidh Belalia <16493223+s77rt@users.noreply.github.com>
2025-04-05 22:34:32 +0100

    export sodium and helpers

--- js/index.ts
+++ js/index.ts
@@ -1,1 +1,10 @@
 import "./NativeRTNSodium";
+export * from "./helpers";
+
+if (!globalThis.__s77rt__sodiuma) {
+	console.error("[@s77rt/react-native-sodium] sodiuma object not found");
+}
+
+// TODO(s77rt): Copy & cache host object properties
+
+export default globalThis.__s77rt__sodiuma;
